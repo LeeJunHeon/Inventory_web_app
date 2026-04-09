7f0b43ae0a617e0f2f25da1355a84d2b56a2eddd
LeeJunHeon <wnsgjsno1@naver.com>
2026-04-09 16:31:13 +0900

    deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-export BUILDX_GIT_INFO=0
 cd /volume1/docker/inventory-web
 git pull
-sudo docker compose build nextjs
+sudo BUILDX_GIT_INFO=0 docker compose build nextjs
 sudo docker compose up -d
 echo "배포 완료!"
