69a959a8c0ac909907a01f7ad0cba952c0d50e4c
LeeJunHeon <wnsgjsno1@naver.com>
2026-06-22 11:12:38 +0900

    fix(auth): basePath 환경에서 SessionProvider가 올바른 인증 경로를 읽도록 basePath 지정

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -4,8 +4,9 @@ import { LanguageProvider } from "@/lib/i18n";
 import BasePathFetch from "@/components/BasePathFetch";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
+  const bp = process.env.NEXT_PUBLIC_BASE_PATH || "";
   return (
-    <SessionProvider>
+    <SessionProvider basePath={`${bp}/api/auth`}>
       <BasePathFetch />
       <LanguageProvider>
         {children}
